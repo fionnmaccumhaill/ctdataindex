a55e1a8fb8ff431733d1f12740686db05c859f69
fionnmaccumhaill <edward.foley@comcast.net>
2016-04-20 20:48:15 -0400

    final changes for v1.0.0

--- test/test1.js
+++ test/test1.js
@@ -1,10 +1,27 @@
 'use strict';
 
 var myidx = require('../lib/index.js');
+var assert = require('assert');
+
+/***/
 var y = myidx.getSocrataList();
 y.dsPromise.then(function (x1) {
     var resourceCnt = x1.dataset.length;
-    console.log('resources:'+resourceCnt);
+    console.log('success - ct datasets count:'+resourceCnt);
+    assert(resourceCnt>300);
+   // console.log(x1);
+}).catch(function (e) {
+    throw(e);
+});
+/****/
+
+// run with domain parameter
+var y1 = myidx.getSocrataList('data.seattle.gov');
+y1.dsPromise.then(function (x1) {
+    var resourceCnt = x1.dataset.length;
+    console.log('success - seattle datasets count:'+resourceCnt);
+    assert(resourceCnt>300);
+    // console.log(x1);
 }).catch(function (e) {
     throw(e);
 });
